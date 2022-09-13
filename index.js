b3cac6b0446bf764b691ffea05d095148b7ae4fa
albert-axi <albert@academyxi.com>
2022-09-13 12:38:12 +0930

    commit tree bc82b658b4c8d6b7d3765a879e1bd49ba48428ad

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 62, num2 = 1
+const multiply = num1 * num2
 
-// Write your code here
+const random = parseInt(Math.random()*10)
+
+const num3 = 24, num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(2,3,6,20,15,10)
+
